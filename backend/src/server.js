98a4fba2add170cc1ecd511825d52d06c46da33c
Jahnavi-cheedella_trid <Jahnavi.cheedella@trueid.in>
2026-05-28 00:12:29 +0530

    added vercel configs

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,7 @@ import { setupChatSocket } from "./sockets/chatSocket.js";
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true
 }));
@@ -19,7 +19,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: { 
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: ["GET", "POST"]
   }
 })
